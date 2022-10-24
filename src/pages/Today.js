ee3f323f0b7b009c50e88f6cabf23806d5248fc6
jepariz <je.parizotto@gmail.com>
2022-10-24 12:50:17 -0300

    criei o card pra cada hábito do dia

--- src/pages/Today.js
+++ src/pages/Today.js
@@ -1,18 +1,17 @@
-import TopBar from "../components/TopBar"
-import Footer from "../components/Footer"
-import styled from "styled-components"
-import { useContext, useEffect } from "react";
+import TopBar from "../components/TopBar";
+import Footer from "../components/Footer";
+import styled from "styled-components";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import MyContext from "../contexts/MyContext";
-import DayJS from 'react-dayjs';
-
-
+import dayjs from "dayjs";
+import check from "../assets/check.png"
 
 export default function Today() {
+  dayjs.locale("pt-br");
 
-
-  const { token, setHabitList } =
-  useContext(MyContext);
+  const { token, setHabitList } = useContext(MyContext);
+  const [habitData, setHabitData] = useState([])
 
   useEffect(() => {
     const promise = axios.get(
@@ -24,7 +23,7 @@ export default function Today() {
       }
     );
     promise.then((res) => {
-      console.log(res.data);
+      setHabitData(res.data)
     });
 
     promise.catch((err) => {
@@ -32,51 +31,85 @@ export default function Today() {
     });
   }, []);
 
-
   return (
-   <>
-    <TopBar/>
-   <TodayContainer>
-   <ProgressContainer>
-    <DayJS></DayJS>
-    <p>Nenhum hábito concluído ainda</p>
-   </ProgressContainer>
-  </TodayContainer>
-  <Footer />
-   
-   </>
-  
-  
-  )
+    <>
+      <TopBar />
+      <TodayContainer>
+        <ProgressContainer>
+          <h3>{dayjs("2022-10-24").format("dddd, DD/MM")}</h3>
+          <p>Nenhum hábito concluído ainda</p>
+        </ProgressContainer>
+        {habitData.map((h, index) =>  
+        <HabitContainer key={habitData[index].id}>
+          <div>
+            <h3>{habitData[index].name}</h3>
+            <p>Sequência atual: 3 dias</p>
+            <p>Seu recorde: 5 dias</p>
+          </div>
+          <CheckButton><img src={check} alt="hábito concluído"></img></CheckButton>
+        </HabitContainer> )}
+       
+      </TodayContainer>
+      <Footer />
+    </>
+  );
 }
 
 const TodayContainer = styled.div`
-
-width: 100%;
-background-color: #F2F2F2;
-box-sizing: border-box;
-padding: 20px;
-
-
-`
-
-
+  width: 100%;
+  background-color: #f2f2f2;
+  box-sizing: border-box;
+  padding: 20px;
+`;
 const ProgressContainer = styled.div`
-margin-top: 80px;
-width: 100%;
+  margin-top: 80px;
+  width: 100%;
+
+  h3 {
+    font-size: 23px;
+    color: #126ba5;
+  }
+
+  p {
+    font-size: 18px;
+    color: #bababa;
+    margin-top: 8px;
+    margin-bottom: 30px;
+  }
+`;
+
+const HabitContainer = styled.div`
+width: 340px;
+height: 94px;
+border: 1px solid #E7E7E7;
+background-color: #fff;
+border-radius: 5px;
+margin-top: 10px;
+display: flex;
+align-items: center;
+justify-content: space-between;
 
 
 h3{
-    font-size: 23px;
-    color: #126BA5;
+  font-size:20px;
+  color: #666666;
+  margin-bottom: 8px;
 }
 
 p{
-    font-size: 18px;
-    color: #BABABA;
-    margin-top: 5px;
+  font-size: 13px;
+  color: #666666;
 }
 
+`
 
-
+const CheckButton = styled.div`
+width: 69px;
+height: 69px;
+border-radius: 5px;
+background-color: #EBEBEB;
+border: 1px solid #E7E7E7;
+display: flex;
+align-items: center;
+justify-content: center;
 `
